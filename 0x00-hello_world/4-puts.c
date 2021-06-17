c39f2de5dfbc1818a7b4f9bd29a98d98b636dd58
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-17 08:32:45 -0700

    main description failure

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: when the program has been executed
+ */
 int main(void)
 	{
-puts "\"Programming is like building a multilingual puzzle"
+puts "\"Programming is like building a multilingual puzzle";
 return (0);
 	}

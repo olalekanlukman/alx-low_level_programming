c5b9e9c3bc087a9de3d745ad84b7d92834f5ee75
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-18 11:58:23 -0700

    reverse

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 	while (n <= 57)
 	{
 		putchar(n);
-		ch++;
+		i++;
 	}
 	while (ch <= 'f')
 	{
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio>
+
+/**
+ * main - this will print numbers with commas
+ * Return: this outputs 0
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		if (n == 57)
+		{
+			putchar(n);
+		}
+		else
+		{
+			putchar(n);
+			putchar(44);
+			putchar(32);
+		}
+		n++;
+	}
+	return (0);

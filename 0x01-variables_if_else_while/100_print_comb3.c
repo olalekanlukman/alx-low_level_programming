037a26d6ba8337fe2db5c7e439d697a70920d260
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-18 22:47:40 -0700

    combination of two num

---
+++ 0x01-variables_if_else_while/100_print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio>
+
+/**
+ * main - this will output combination of two numbers with
+ * out repeating
+ * Return: this outputs 0
+ */
+
+int main(void)
+{
+	int a = 48;
+	int b = 49;
+	int j = 49;
+
+	while (a <= 57)
+	{
+		while (b <= 57)
+		{
+			putchar(a);
+			putchar(b);
+			if (a != 56 || b != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			b++;
+		}
+	b = (b / b) + j;
+	a++;
+	j++;
+	}
+	putchar("\n");
+	return (0);
+	}

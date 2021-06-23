ccbee800cfa29ff592568bba151514ac1b3d1f8b
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-23 08:37:42 -0700

    add equality sign

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 		_putchar(' ');
 		_putchar(' ');
 		}
-		else if (p > 9 && b != 9)
+		else if (p >= 9 && b != 9)
 		{
 		_putchar((p / 10) + '0');
 		_putchar((p % 10) + '0');

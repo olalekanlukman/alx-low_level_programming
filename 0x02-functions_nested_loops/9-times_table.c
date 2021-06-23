84dea808da6b7e164d9bb8ab064784d1ea8ec0e9
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-23 07:48:06 -0700

    build a 9time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+ * times_table - this will print 9 times table
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int a = 0, b, p;
+
+	while (a <= 9)
+{
+	b = 0;
+	while (b <= 9)
+	{
+		p = a * b;
+		if (p < 9 && b != 9)
+		{
+		_putchar(p + '0');
+		_putchar(p + '0');
+		_putchar(' ');
+		_putchar(' ');
+		}
+		else if (p > 9 && b != 9)
+		{
+		_putchar((p / 10) + '0');
+		_putchar((p % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		}
+		else if (b == 9 && p < 10)
+		{
+		_putchar(p + '0');
+		_putchar('\n');
+		}
+		else if (b == 9 && p > 10)
+		{
+		_putchar((p / 10) + '0');
+		_putchar((p % 10) + '0');
+		_putchar('\n');
+		}
+		b++;
+		}
+		a++;
+		b = 0;
+	}
+}

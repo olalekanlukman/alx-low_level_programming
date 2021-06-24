2de4b2d66122e7248f3840582e53389c9c80cf51
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-24 11:41:26 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,16 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	if (b > a && b > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	if (c > a && c > b)
+	else if (c > a && c > b)
 	{
 		largest = c;
+	}
+	else
+	{
 	}
 
 	return (largest);

a52f986423fb883f992974d9e04fcf85037546fe
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-25 10:00:30 -0700

    sum of 3 & 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this prints out the sum of all multiples of3 and 5 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	int sum = 0;
+
+	while (a < 1024)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			sum += a;
+		}
+		a++;
+	}
+	printf("%d", sum);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints out the integer with putchar
+ * @n: this is the integer
+ * Return: 0
+ */
+

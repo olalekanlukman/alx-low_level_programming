6d5a45c1dde2cdf24728b9ab5064663464dfc25c
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-25 05:23:39 -0700

    triangle made

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,3 +8,26 @@
 
 void print_triangle(int size)
 {
+	int a = 0, b = 0, j = 0;
+
+	while (b < size)
+	{
+		while (a < size - 1)
+		{
+			_putchar(' ');
+			a++;
+		}
+		while (a < size + j)
+		{
+			_putchar('#');
+			a++;
+		}
+		j++;
+		_putchar('\n');
+		b++;
+		a = b;
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
+

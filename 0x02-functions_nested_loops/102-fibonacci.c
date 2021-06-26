1ec3d84e47d616e443eeb0b96ced4fa046303a8e
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-25 23:07:29 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints 50 fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	unsigned long int nextnum;
+	int i = 3;
+
+	printf("%d", a);
+	printf(", %d", b);
+	while (i <= 50)
+	{
+		nextnum = a + b;
+		printf(", %lu", nextnum);
+		a = b;
+		b = nextnum;
+		i++;
+	}
+	printf("\n");
+}

5892f66a80f6230232dec979c440d3dde08de9e9
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-29 09:32:20 -0700

    get the lenght

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,8 +12,10 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i++;
 	}
+	len = i - 1;
+	for (j = 0; j <= len; j++)
+		putchar(*(str + j));
+
 	_putchar('\n');
 }

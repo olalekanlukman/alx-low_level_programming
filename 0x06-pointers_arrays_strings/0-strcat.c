8ccdc9af1e4b8f8c370b7a3bffee34f6913e11a0
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-06-30 11:01:59 -0700

    string concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: is the final string
+ * @src: this is the appending string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j, len;
+
+	for (i = 0; *(dest + i) != '\0'; i++)
+	{
+	}
+	for (j = 0; *(src + j) != '\0'; j++)
+	{
+	}
+	len = j;
+	for (j = 0; j <= len; j++)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strncat - concatenates two strings to a limit
+ * @dest: final string
+ * @src: appending string
+ * @n: length of src
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; *(dest + i) != '\0'; i++)
+	{
+	}
+

90201eb3c712e55ec0737f43127886735770bebc
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-07-01 11:43:11 -0700

    roted

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rot13 - prints encode and decode of rot13
+ * @str: this is the string to be ROT
+ * Return: str
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}

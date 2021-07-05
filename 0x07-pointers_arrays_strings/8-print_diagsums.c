23faf255f48218364baa56a2fde5010eda642ab3
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-07-05 14:05:39 -0700

    add initialization

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,12 +11,12 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	unsigned int sum, sum1;
+	unsigned int sum = 0, sum1 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		sum += a[(size * i) + i];
-		sum1 += a[(size * (i + 1)) - (i + 1)};
+		sum1 += a[(size * (i + 1)) - (i + 1)];
 	}
 	printf("%d, %d\n", sum, sum1);
 }

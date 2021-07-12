e1d3ab0585c599bd0a91ed7445dcfbfa4ea7e922
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-07-12 08:22:47 -0700

    no loop

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints multiplication of two numbers
+ * @argc: counts the variables
+ * @argv: array
+ * Return: 0;
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{	
+	int x, y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x =strtol(argv[1], NULL, 10);
+	y =strtol(argv[2], NULL, 10);
+	printf("%d\n", (x * y));
+	return (0);
+}

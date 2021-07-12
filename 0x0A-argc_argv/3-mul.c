76d76c0e477dbafe6f5339155b218ddad8115a45
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-07-12 09:34:11 -0700

    add greater than

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,16 @@
  */
 
 int main(__attribute__((unused)) int argc, char *argv[])
-{	
+{
 	int x, y;
 
-	if (argc < 3)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	x =strtol(argv[1], NULL, 10);
-	y =strtol(argv[2], NULL, 10);
+	x = strtol(argv[1], NULL, 10);
+	y = strtol(argv[2], NULL, 10);
 	printf("%d\n", (x * y));
 	return (0);
 }

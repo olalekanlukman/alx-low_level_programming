afbee583b014b9128df1011a505d402bce27c335
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-07-13 13:41:21 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, len1, len2;
 	char *p;
 
-	if (s1 = NULL)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 = NULL)
+	if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;

c7981683e68ec053a9e7911511b68226a4075b6d
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-07-15 10:04:02 -0700

    maxmin

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,9 +11,14 @@
 int *array_range(int min, int max)
 {
 	int *p, i;
+	
+	if (min > max)
+		return (NULL);
 
 	p = malloc((max - min + 1) * sizeof(int));
-	for (i = 0; i <= (max - min); i++)
-		p[i] = i;
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++)
+		p[i] = min++;
 	return (p);
 }

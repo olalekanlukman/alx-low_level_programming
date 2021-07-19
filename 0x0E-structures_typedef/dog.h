433b93554c51cf028b8600c4125920df928f3124
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-07-19 16:18:10 -0700

    structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - collects properties of a dog
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

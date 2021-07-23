4e1e8295720255ef81ca49995fd1ac096a4ed00e
Lukman <olalekanlukmanmohammed@yahoo.com>
2021-07-23 15:36:51 -0700

    double inclusion corrected

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,12 @@ struct dog
 };
 
 typedef struct dog dog_t;
-#endif
+
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
